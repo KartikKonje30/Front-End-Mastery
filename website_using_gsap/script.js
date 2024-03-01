3a858c10637739901820dca6d75363055f36e7b6
KartikKonje30 <kartikkonje.01@gmail.com>
2024-03-01 18:17:32 +0530

    1st commit

--- website_using_gsap/script.js
+++ website_using_gsap/script.js
@@ -46,3 +46,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left: dets.x,
+        top: dets.y
+    })
+})
+
+document.querySelectorAll(".child").forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to("#cursor",{
+            transform: "translate(-50%,-50%) scale(1)",
+        });
+    });
+    elem.addEventListener("mouseleave",function(){
+        gsap.to("#cursor",{
+            transform: "translate(-50%,-50%) scale(0)",
+        });
+    });
+})
