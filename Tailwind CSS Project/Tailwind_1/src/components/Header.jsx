2c72883a695ee91171791b867d7475e439748ba0
KartikKonje30 <kartikkonje.01@gmail.com>
2024-06-03 20:51:42 +0530

    6th commit

--- Tailwind CSS Project/Tailwind_1/src/components/Header.jsx
+++ Tailwind CSS Project/Tailwind_1/src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <div>
+        <img src="./assets/Logo.svg" alt="Logo" />
+        <button>Hoister is hiring</button>
+      </div>
+      <div>
+        <FaBars />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
