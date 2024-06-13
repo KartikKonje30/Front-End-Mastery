ef7561c28d812cdb1de878f793caee989cd7448e
KartikKonje30 <kartikkonje.01@gmail.com>
2024-06-13 18:28:24 +0530

    3rd commit

--- Tailwind CSS Project/Tailwind_2/src/App.jsx
+++ Tailwind CSS Project/Tailwind_2/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-     <div className="bg-slate-900 min-h-screen">
-      <h1 className="text-6xl text-sky-400">Tailwind Practice 2</h1>
+     <div className="bg-slate-900 min-h-screen text-center">
+      <h1 className="text-6xl text-sky-400 py-6 tracking-tight font-sans">tailwindcss</h1>
      </div>
     </>
   )
--- Tailwind CSS Project/Tailwind_2/tailwind.config.js
+++ Tailwind CSS Project/Tailwind_2/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: "'Inter', san-serif",
+      }
+    },
   },
   plugins: [],
 }
