220be12418f310395490396f013fc7b239253de2
KartikKonje30 <kartikkonje.01@gmail.com>
2025-04-18 08:53:54 +0530

    todo half done

--- React-Redux Practice/Todo-List/src/Todo.jsx
+++ React-Redux Practice/Todo-List/src/Todo.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, deleteTask } from './store';
+
 
 const Todo = () => {
+
+    const [task, setTask] = useState('');
+
+    const tasks = useSelector((state) => state.task);
+    const dispatch = useDispatch();
+
+
+    const handleSubmitTask = (e) => {
+        e.preventDefault();
+        if(task === ''){
+            window.alert('Enter Valid Task!')
+            return setTask('');
+        }
+        dispatch(addTask(task))
+        return setTask('');
+    }
+
+    const handleDeleteTask = (i) => {
+        return  dispatch(deleteTask(i))
+    }
+
+
   return (
-    <div>Todo</div>
+    <div>
+        <div className='todo-card'>
+
+        <div className="title">
+
+            <div className=''>
+            <h1 className='font-Epilogue'>TO-DO</h1>
+            <h2 className='font-Epilogue'>Keep it up!</h2>
+            </div>
+
+            <div className='tasks-display'>
+            <p className='font-Epilogue tasks-count'>0/{tasks.length}</p>
+            </div>
+
+        </div>
+
+        <form className="input-section" onSubmit={handleSubmitTask}>
+        <input type="text"  placeholder="Enter your Task" className="font-Epilogue input-box" value={task} onChange={(e) => setTask(e.target.value) } />
+        <button className='input-button'>+</button>
+        </form>
+
+        <div className=''>
+        <ul className="font-Epilogue">
+            {
+                tasks.map((currTask, index) => {
+                return <li className='font-Epilogue' key={index}>
+                <div className='task-input'>
+                <label className='input-checkbox' htmlFor=''/>
+                {currTask}
+                </div>
+                <div className='task-input'>
+                <FaEdit size={24} color='#848DFF' /> 
+                <FaTrashAlt size={24} color='#f33f3f' onClick={() => handleDeleteTask(index)}/> 
+                </div>
+                </li>
+
+                })
+            }
+            {/* <li className='font-Epilogue'>
+                <div className='task-input'>
+                <label className='input-checkbox' htmlFor=''/>
+                Hii 
+                </div>
+                <div className='task-input'>
+                <FaEdit size={24} color='#848DFF' /> 
+                <FaTrashAlt size={24} color='#f33f3f' /> 
+                </div>
+            </li> */}
+        </ul>
+        </div>
+        
+        </div>
+    </div>
   )
 }
 
--- React-Redux Practice/Todo-List/src/store.jsx
+++ React-Redux Practice/Todo-List/src/store.jsx
@@ -40,33 +40,40 @@ const taskReducer = (state = initialState, action) => {
 export const store = createStore(taskReducer);
 
 // getState() used to get the current state of the store
-console.log("initial State: ", store.getState());
+// console.log("initial State: ", store.getState());
 
 // dispatch task 
 store.dispatch({ type: ADD_TASK, payload: "Learn Redux"});
-console.log("updated State: ", store.getState());
+// console.log("updated State: ", store.getState());
 
 store.dispatch({ type: ADD_TASK, payload: "Learn Redux + Redux Toolkit"});
-console.log("updated State: ", store.getState());
+// console.log("updated State: ", store.getState());
 
 store.dispatch({ type: DELETE_TASK, payload: 1});
-console.log("After Delete State: ", store.getState());
+// console.log("After Delete State: ", store.getState());
 
 // Action Creators : Improves code readability
 
-function addTask(data) {
+export function addTask(data) {
     return { type: ADD_TASK, payload: data}
 }
 
-function deleteTask(id) {
+export function deleteTask(id) {
     return { type: DELETE_TASK, payload: id}
 }
 
 store.dispatch(addTask("Learn Action creators"))
-console.log("Updated State: ", store.getState());
+// console.log("Updated State: ", store.getState());
 
 store.dispatch(deleteTask(1))
-console.log("After Delete State: ", store.getState());
+// console.log("After Delete State: ", store.getState());
+
+store.dispatch(addTask('Created reducer function'));
+
+store.dispatch(addTask('Created store and passed reducer function'));
+
+store.dispatch(addTask('dispatched tasks into the store'));
+
 
 
 
